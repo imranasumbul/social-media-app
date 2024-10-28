1d5170673ef46b49abf7a6f9862ce95839d1bfe6
Imrana Sumbul <imranasumbul1211@gmail>
2024-10-28 20:39:27 +0530

    prismadb changed

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,16 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-
-// Declare global variable without using 'namespace' or 'var'
+import { PrismaClient } from "@prisma/client"
 declare global {
-  // Use 'let' instead of 'var' for better scope control
-  var prisma: PrismaClient | undefined;
+    var prisma : PrismaClient | undefined
 }
-
-const client = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  // Assign the client to global.prisma only if it's not in production
-  global.prisma = client;
-}
-
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
 export default client;
